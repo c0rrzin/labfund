503115ce0eaef7f6dd8c04b3fb78608ca165569c
Renan T. Hangai <renan.hangai@paysession.net>
2013-09-26 12:46:37 -0300

    Adicionado arquivo UsedVars.mk para facilitar o Makefile

---
+++ UsedVars.mk
@@ -0,0 +1,13 @@
+# Variaveis utilizadas
+JAVA?=java
+MLR?="${TOPDIR}/MLR.jar"
+MVN?="${TOPDIR}/mvn2013.jar"
+CP?=cp
+MV?=mv
+MKDIR?=mkdir -p
+RM?=rm -f
+CURSPACES:="$(SPACES)"
+SPACES:="$(CURSPACES)  "
+
+# Exporta as variaveis
+export JAVA MLR MVN CP MV MKDIR SPACES 
