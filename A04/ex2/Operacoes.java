4b6752353325662e4383fb312343f5581ea0655c
Renan T. Hangai <renan.hangai@paysession.net>
2013-10-03 11:52:21 -0300

    Adicionado arquivos do exercicio 2 extraidos do Codigos.zip

---
+++ A04/ex2/Operacoes.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**
+ * Classe abstrata que representa as operações
+ *
+ */
+public abstract class Operacoes {
+	/**
+	 * Método que calculará as operações para cada classe
+	 * @param operando1 operando 1 da operação
+	 * @param operando2 operando 2 da operação
+	 * @return resultado da operação
+	 */
+	public abstract float calculaOperacao(float operando1, float operando2);
+}
