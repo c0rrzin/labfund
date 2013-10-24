15fdf783ce175408ad5e1c49a504d62224bc2ecc
Renan T. Hangai <renan.hangai@paysession.net>
2013-10-23 22:38:50 -0200

    Adicionado arquivos do exercicio1 da aula07

---
+++ A07/E01/T5G12A07E01_GETLINEF.asm
@@ -0,0 +1,97 @@
+; GETLINEF
+
+
+GETLINEF >
+GL_END >
+GL_UL >
+GL_BUF >
+
+CONST_GD <
+CONST_STORE <
+CONST_1 <
+CONST_2 <
+
+& /0100
+
+GL_CONST_EOL K /0D0A
+GL_CONST_EOF K /FFFF
+
+GL_END K /0000
+GL_UL K /0000
+GL_BUF K /0000
+
+GL_IS_EOF        K /0000
+GL_CURRENT       K /0000
+GL_CONST_300     K /0300
+
+GETLINEF K /0000
+	; Cria a instrução get data
+	LD GL_UL
+	+ GL_CONST_300
+	+ CONST_GD
+	MM GETLINEF_GD_INSTR
+
+	; Zera o parametro EOF
+	LV /0000
+	MM GL_IS_EOF
+
+	; Retorna se o buffer é vazio
+	LD GL_BUF
+	JZ GETLINEF_RET
+
+; Loop de leitura
+GETLINEF_LOOP $ =0
+	; Verifica o tamanho do buffer
+	LD GL_BUF
+	- CONST_1
+	JZ GETLINEF_EOS
+	MM GL_BUF
+
+	; Cria a instrução de salvar no endereço
+	LD GL_END
+	+ CONST_STORE
+	MM GETLINEF_STORE_INSTR
+	MM GETLINEF_STORE_EOS_INSTR
+
+	; Lê o disco e salva no buffer e em GL_CURRENT
+GETLINEF_GD_INSTR K /0000
+	MM GL_CURRENT
+
+	; Fim do arquivo FFFF
+	- GL_CONST_EOF
+	JZ GETLINEF_EOF
+
+	; Fim da linha
+	LD GL_CURRENT
+	- GL_CONST_EOL
+	JZ GETLINEF_EOS
+
+	; Salva o byte lido e continua
+	LD GL_CURRENT
+GETLINEF_STORE_INSTR K /0000
+	LD GL_END
+	+ CONST_2
+	MM GL_END
+	JP GETLINEF_LOOP
+
+; Fim da linha
+GETLINEF_EOL $ =0
+	JP GETLINEF_EOS
+
+; Fim do arquivo
+GETLINEF_EOF $ =0
+	LV /0001
+	MM GL_IS_EOF
+	JP GETLINEF_EOS
+
+; Coloca o fim da string
+GETLINEF_EOS $ =0
+	LV /0000
+GETLINEF_STORE_EOS_INSTR K /0000
+	
+; Fim da função
+GETLINEF_RET $ =0
+	LD GL_IS_EOF
+RS GETLINEF
+
+# GETLINEF
---
+++ A07/E01/T5G12A07E01_const.asm
@@ -0,0 +1,43 @@
+;========================================
+; Arquivo de constantes
+;========================================
+
+CONST_1 >
+CONST_2 >
+CONST_6 >
+CONST_A >
+CONST_10 >
+CONST_100 >
+CONST_1000 >
+CONST_LOAD >
+CONST_STORE >
+CONST_GD >
+CONST_PD >
+CONST_OS >
+CONST_FFFF >
+CONST_H2A_0 >
+CONST_H2A_A >
+
+; Endereço
+& /0000
+
+; Constantes numericas
+CONST_1      K /1
+CONST_2      K /2
+CONST_6      K /6
+CONST_A      K /A
+CONST_10     K /10
+CONST_100    K /100
+CONST_1000   K /1000
+CONST_LOAD   K /8000
+CONST_STORE  K /9000
+CONST_GD     K /D000
+CONST_PD     K /E000
+CONST_OS     K /F000
+CONST_FFFF   K /FFFF
+
+; Constantes para conversão Hex->Ascii
+CONST_H2A_0  K /30
+CONST_H2A_A  K /41
+
+# CONST_1
