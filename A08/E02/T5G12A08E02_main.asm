19dd4d42dc496d91566a72078c9de7054fe5b0d3
renanhangai <hangai.renan@gmail.com>
2013-10-31 14:56:47 -0200

    Corrigido unidade logica do main

--- A08/E02/T5G12A08E02_main.asm
+++ A08/E02/T5G12A08E02_main.asm
@@ -5,18 +5,18 @@ DUMP_UL 	<
 DUMP_BL 	<
 DUMP_EXE 	<
 
-;========================= NÃO ALTERE ESTE TRECHO: INÍCIO ===================;
+;========================= NÃƒO ALTERE ESTE TRECHO: INÃCIO ===================;
 & /0000
-MAIN			JP	START	; Salta para início do main
+MAIN			JP	START	; Salta para inÃ­cio do main
 
 ; PARAMETROS
-END_INICIAL		K	/0400	; Endereço onde começa o dump
+END_INICIAL		K	/0400	; EndereÃ§o onde comeÃ§a o dump
 TAMANHO_TEST	K	/0032	; Numero total de palavras a serem "dumpadas"
-UL_TEST			K	/0001	; Unidade logica do disco a ser usado
+UL_TEST			K	/0000	; Unidade logica do disco a ser usado
 BL_TEST 		K	/0010	; Tamanho do bloco
-EXE_TEST 		K	/0400	; Endereço onde começaria a execução (valor dummy, apenas para manter o formato)
+EXE_TEST 		K	/0400	; EndereÃ§o onde comeÃ§aria a execuÃ§Ã£o (valor dummy, apenas para manter o formato)
 
-;========================== NÃO ALTERE ESTE TRECHO: FIM =====================;
+;========================== NÃƒO ALTERE ESTE TRECHO: FIM =====================;
 
 START			LD	END_INICIAL	; Parametros, na ordem acima
 				MM	DUMP_INI
@@ -33,3 +33,4 @@ FIM_MAIN		HM	FIM_MAIN	; Fim do main
 
 # MAIN
 				
+PD /301 # E300 + UL
