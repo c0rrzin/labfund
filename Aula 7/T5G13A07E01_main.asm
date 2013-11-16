60efad2c348998fb2bcc8e8b53508c6ef5c74dcf
Thales Machado <thales.mchd@gmail.com>
2013-11-15 23:06:06 -0200

    finished aula 7

--- Aula 7/T5G13A07E01_main.asm
+++ Aula 7/T5G13A07E01_main.asm
@@ -12,14 +12,17 @@ VAL_UL  K   /0001   ; UL do arquivo
 VAL_BUF K   =10     ; Tamanho do buffer
 BUFFER  $   /000A   ; Buffer: algumas posi��es reservadas
 
-
-START   LD  VAL_BUF     ; Param 1: tamanho do buffer
-        MM  GL_BUF
-        LD  VAL_UL    ; Param 2: unidade l�gica
-        MM  GL_UL
-        LV  BUFFER    ; Param 3: endere�o do buffer
-        MM  GL_END
-        SC  GETLINEF  ; Chama subrotina
+START    GD /301    ;
+         JP DOOS
+         K  /0001    ;parametro a ser passado
+DOOS     OS /01FF
+;        LD  VAL_BUF     ; Param 1: tamanho do buffer
+;        MM  GL_BUF
+;        LD  VAL_UL    ; Param 2: unidade l�gica
+;        MM  GL_UL
+;        LV  BUFFER    ; Param 3: endere�o do buffer
+;        MM  GL_END
+;        SC  GETLINEF  ; Chama subrotina
 END     HM  END     ; fim do programa
 
 # MAIN
